92442e4aee825d2cbec16b69f9e676953e15d419
Jennifer Spicer <xJenniferSpicer@gmail.com>
2023-11-14 22:39:01 -0600

    Advanced State Management Part 1

---
+++ blog/Blog/src/screens/IndexScreen.js
@@ -0,0 +1,24 @@
+import React, {useContext} from 'react';
+import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
+import BlogContext from "../context/BlogContext";
+
+const IndexScreen = () => {
+
+    const {data, addBlogPost} = useContext(BlogContext);
+
+    return <View>
+        <Button title="Add Post" onPress={() => { addBlogPost() }} />
+        <FlatList
+            data = {data}
+            keyExtractor={(blogPost) => {return blogPost.title}}
+            renderItem={ ({item}) => {
+                return <Text>{item.title}</Text>
+            }}
+        />
+        <Text> Index Screen!</Text>
+    </View>
+}
+
+const styles = StyleSheet.create({});
+
+export default IndexScreen;
