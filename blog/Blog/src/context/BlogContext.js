25d8ec2e0facaed1c02fdd8af68bef0bfb70e0f2
Jennifer Spicer <xJenniferSpicer@gmail.com>
2023-11-17 01:05:15 -0600

    Modified Screen

--- blog/Blog/src/context/BlogContext.js
+++ blog/Blog/src/context/BlogContext.js
@@ -1,17 +1,32 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
+import createDataContext from "./createDataContext";
 
-const BlogContext = React.createContext();
-
-export const BlogProvider = ( {children} ) => {
-
-    const [blogPosts, setBlogPosts] = useState([]);
-
-    const addBlogPost = () => {
-        setBlogPosts([...blogPosts, {title: `Blog Post #${blogPosts.length + 1}`}]);
+const blogReducer = (state, action) => {
+    switch(action.type){
+        case "add_blogpost":
+            return [...state, {id: Math.floor(Math.random() * 9999), title: `Blog Post #${state.length + 1}`}]
+        case "delete_blogpost":
+            return state.filter((blogPost) => {
+                //return true if the id is NOT the one we want to delete
+                //this will return a new state of all blogs we do not want to delete, effectively "deleting" the payload
+                return blogPost.id !== action.payload
+            });
+        default:
+            return state;
     }
-    return <BlogContext.Provider value={ {data: blogPosts, addBlogPost: addBlogPost} }>
-        {children}
-    </BlogContext.Provider>
-};
+}
 
-export default BlogContext;
+const addBlogPost = (dispatch) => {
+    return () => {
+        dispatch({type: "add_blogpost"})
+    }
+}
+
+const deleteBlogPost = (dispatch) => {
+    return (id) => {
+        dispatch({type: "delete_blogpost", payload: id})
+    }
+}
+
+//allow other files in our code to access Context and Provider
+export const {Context, Provider} = createDataContext(blogReducer, {addBlogPost: addBlogPost, deleteBlogPost: deleteBlogPost}, []);
--- blog/Blog/src/screens/IndexScreen.js
+++ blog/Blog/src/screens/IndexScreen.js
@@ -1,24 +1,59 @@
-import React, {useContext} from 'react';
+import React, { useContext } from 'react';
 import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
-import BlogContext from "../context/BlogContext";
+import {Context} from "../context/BlogContext";
+import { Feather } from "@expo/vector-icons";
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
-const IndexScreen = () => {
+const IndexScreen = (props) => {
 
-    const {data, addBlogPost} = useContext(BlogContext);
+    //useContext gets the data from the passed in context, and stores the data in the repestive variable
+    const {state, addBlogPost, deleteBlogPost} = useContext(Context);
 
     return <View>
         <Button title="Add Post" onPress={() => { addBlogPost() }} />
         <FlatList
-            data = {data}
+            data = {state}
             keyExtractor={(blogPost) => {return blogPost.title}}
             renderItem={ ({item}) => {
-                return <Text>{item.title}</Text>
+                return <TouchableOpacity onPress={ () => {props.navigation.navigate("Show", {id: item.id})}}>
+                            <View style={styles.row}>
+                                <Text style={styles.title}>{item.title} -- {item.id}</Text>
+                                <TouchableOpacity onPress={() => {deleteBlogPost(item.id)}}>
+                                    <Feather style={styles.icon} name="trash"/>
+                                </TouchableOpacity>
+                            </View>
+                        </TouchableOpacity>
             }}
         />
-        <Text> Index Screen!</Text>
     </View>
 }
 
-const styles = StyleSheet.create({});
+IndexScreen.navigationOptions = (props) => {
+    return {
+        headerRight: () => (
+            <TouchableOpacity onPress={ () => {props.navigation.navigate("Create")}}>
+                <Feather name="plus" size={30} />
+            </TouchableOpacity>
+        ),
+    };
+}
+
+const styles = StyleSheet.create({
+    row : {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        paddingVertical: 20,
+        paddingHorizontal: 20,
+        borderTopWidth: 1,
+        borderBottomWidth: 1,
+        borderColor: "grey",
+    },
+    title:{
+        fontSize:18,
+    },
+    icon:{
+        fontSize: 24,
+    }
+});
 
 export default IndexScreen;
