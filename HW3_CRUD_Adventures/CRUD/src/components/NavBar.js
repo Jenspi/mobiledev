a88f926fa303504d42e647257cfabfb20b766048
Jennifer Spicer <xJenniferSpicer@gmail.com>
2023-11-24 18:27:45 -0600

    Cleaned up file

--- HW3_CRUD_Adventures/CRUD/src/components/NavBar.js
+++ HW3_CRUD_Adventures/CRUD/src/components/NavBar.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { View, Text, StyleSheet, Button, TextInput } from "react-native";
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import { withNavigation } from "react-navigation";
 
@@ -7,7 +7,6 @@ const NavBar = (props) => {
     return <View style={styles.navbar}>
         <TouchableOpacity style={styles.button} onPress={ () => {props.navigation.navigate("Index")}}><Text style={styles.label}>Roster</Text></TouchableOpacity>
         <TouchableOpacity style={styles.button} onPress={ () => {props.navigation.navigate("Adventure")}}><Text style={styles.label}>Adventures</Text></TouchableOpacity>
-        
     </View>
 }
 
@@ -15,27 +14,18 @@ const styles = StyleSheet.create({
     navbar:{
         flexDirection:"row",
         fontSize:20,
-        
-        //height:60,
-        //backgroundColor:"#6023B3",
         backgroundColor:"#5116A1",
         alignItems:"center",
-        //justifyContent:"space-evenly",
         justifyContent:"space-between"
-        //alignContent:"stretch"
     },
     label: {
         fontSize:18,
-        // marginBottom: 5,
-        // marginLeft: 5,
-        //margin:20,
         marginVertical:5,
         marginHorizontal:59
     },
     button: {
         borderWidth: 3,
         borderColor: "black",
-        
     }
 });
 
