a14615ddc856f4246c19c4255d0c6ab42176addc
Jennifer (Jenny) Spicer <xJenniferSpicer@gmail.com>
2025-11-14 16:51:06 -0800

    persistent storage

--- blog/Blog/src/api/jsonServer.js
+++ blog/Blog/src/api/jsonServer.js
@@ -2,5 +2,5 @@ import axios from 'axios'
 
 export default axios.create({
     //our server
-    baseURL: "https://fbc1-152-44-247-185.ngrok-free.app"
+    baseURL: "https://5fe5-152-44-247-185.ngrok-free.app"
 });
